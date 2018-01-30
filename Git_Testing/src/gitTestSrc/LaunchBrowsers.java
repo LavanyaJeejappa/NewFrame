e3e2a6da2427b8f68e5301f0f1d599572c2be1d5
LavanyaJeejappa <lavanya.lav888@gmail.com>
2018-01-31 00:29:08 +0530

    uploading framework to git--inital

---
+++ Git_Testing/src/gitTestSrc/LaunchBrowsers.java
@@ -0,0 +1,14 @@
+package gitTestSrc;
+
+import org.testng.annotations.Test;
+
+public class LaunchBrowsers {
+
+	@Test
+	public void beforemethod()
+	{
+
+        System.out.println("launching all the browsers");
+	}
+
+}
---
+++ Git_Testing/src/gitTestSrc/LaunchFireFox.java
@@ -0,0 +1,45 @@
+package gitTestSrc;
+
+/*import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;*/
+
+import org.testng.annotations.Test;
+
+
+public class LaunchFireFox {
+	
+	@Test
+	public void lauch()
+	{
+		System.out.println("removed the methods");
+	}
+
+	/*WebDriver driver;
+	
+    
+	 @Test
+	  public void RunTestMethod() {
+		 
+		 driver.get("https://www.facebook.com");
+	  }
+	
+	  
+	  @BeforeMethod
+	  public void PreCondition()
+	  {
+			  System.setProperty("webdriver.chrome.driver", "./driver/chromedriver_32.exe");
+			  driver=new ChromeDriver();
+		  
+	  }  
+		 
+	  
+	  @AfterMethod
+	  public void PostCondition() {
+		  
+		 System.out.println("chrome lauched successfully");
+	  }
+*/
+}
